9a65e982457fac74e663a39fc35c0e40100b7789
pgarg6 <pgarg6@ncsu.edu>
2022-02-09 18:36:09 -0500

    Adding User Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   root 'home#index'
   resources :admins
   resources :instructors
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_233502) do
+ActiveRecord::Schema.define(version: 2022_02_09_233340) do
 
   create_table "admins", force: :cascade do |t|
     t.string "admin_id"
@@ -71,6 +71,14 @@ ActiveRecord::Schema.define(version: 2022_02_05_233502) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["student_id"], name: "index_students_on_student_id", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "user_role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "waitlists", force: :cascade do |t|
