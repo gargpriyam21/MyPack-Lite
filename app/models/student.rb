4d9ceacc7b46ed06880d30b1af5b98f67daf7d34
Priyam Garg <gargpriyam21@gmail.com>
2022-02-10 05:42:15 -0500

    Added Session Manager (Basic framework)

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ApplicationRecord
+  belongs_to :user
+  has_many :courses
+  has_many :enrollments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_one :instructor
+  has_one :student
+  has_one :admin
 end
