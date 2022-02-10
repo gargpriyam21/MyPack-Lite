e51189b24b8f77a387c548f1106c9d1f1d2a8f5f
pgarg6 <pgarg6@ncsu.edu>
2022-02-10 05:42:15 -0500

    Added Session Manager (Basic framework)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,40 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user
+      if user.user_role == "student"
+        user_class = Student
+      elsif user.user_role == "instructor"
+        user_class = Instructor
+      elsif user.user_role == "admin"
+        user_class = Admin
+      else
+        user_class = nil
+      end
+      specific_user_object = user_class.find_by_user_id(user.id)
+      if specific_user_object.authenticate(params[:password])
+        session[:user_id] = user.id
+        session[:user_role] = user.user_role
+        redirect_to root_url
+      else
+        flash.now[:alert] = "Email or password is invalid"
+        render "new"
+      end
+    else
+      # puts "LODU"
+      flash.now[:alert] = "No user found"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
+end
+
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,2 +1,3 @@
 class Instructor < ApplicationRecord
+  belongs_to :user
 end
