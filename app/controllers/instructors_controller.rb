9559e277bcb1691f995ebe6c61d1866a33533f02
Dev Mehta <devmehta5799@gmail.com>
2022-02-12 14:38:55 -0500

    Added Check Permission to Application Controller and applied to Instructors and Courses

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,45 +1,55 @@
 class InstructorsController < ApplicationController
-  skip_before_action :authorized, only: [:new, :create]
   before_action :set_instructor, only: %i[ show edit update destroy ]
 
   # GET /instructors or /instructors.json
   def index
+    if !check_permissions?(session[:user_role], "create_instructor")
+      redirect_to root_path
+    end
     @instructors = Instructor.all
   end
 
   # GET /instructors/1 or /instructors/1.json
   def show
+    if !check_permissions?(session[:user_role], "show_instructor")
+      redirect_to root_path
+    end
   end
 
   # GET /instructors/new
   def new
+    if !check_permissions?(session[:user_role], "create_instructor")
+      redirect_to root_path
+    end
     @instructor = Instructor.new
   end
 
   # GET /instructors/1/edit
   def edit
-    # TODO check permissions
+    if !check_permissions?(session[:user_role], "edit_instructor")
+      redirect_to root_path
+    end
   end
 
   # POST /instructors or /instructors.json
   def create
-    email = params[:instructor][:email]
-    user = { :email => email, :user_role => 'instructor' }
-    @instructor = nil
-    @user = User.new(user)
+    if !check_permissions?(session[:user_role], "create_instructor")
+      redirect_to root_path
+    end
 
-    respond_to do |format|
-      if @user.save
-        @instructor = Instructor.new(instructor_params)
-        @instructor.user_id = @user.id
+    email = params[:instructor][:email]
+    userHash = {:email => email, :user_role => "instructor"}
+    @applicant = nil
+
+    @user = User.new(userHash)
+
+    if @user.save!
+      @instructor = Instructor.new(instructor_params)
+      @instructor.user_id = @user.id
+      respond_to do |format|
         if @instructor.save
-          if (not current_user.nil? and current_user.user_role == "admin")
-            format.html { redirect_to @instructor, notice: "Instructor was successfully created by Admin." }
-            format.json { render :show, status: :created, location: @instructor }
-          else
-            format.html { redirect_to login_path, notice: "Instructor was successfully created." }
-            format.json { render :show, status: :created, location: @instructor }
-          end
+          format.html { redirect_to instructor_url(@instructor), notice: "Instructor was successfully created." }
+          format.json { render :show, status: :created, location: @instructor }
         else
           format.html { render :new, status: :unprocessable_entity }
           format.json { render json: @instructor.errors, status: :unprocessable_entity }
@@ -50,6 +60,9 @@ class InstructorsController < ApplicationController
 
   # PATCH/PUT /instructors/1 or /instructors/1.json
   def update
+    if !check_permissions?(session[:user_role], "update_instructor")
+      redirect_to root_path
+    end
     respond_to do |format|
       if @instructor.update(instructor_params)
         format.html { redirect_to instructor_url(@instructor), notice: "Instructor was successfully updated." }
@@ -63,8 +76,11 @@ class InstructorsController < ApplicationController
 
   # DELETE /instructors/1 or /instructors/1.json
   def destroy
+    if !check_permissions?(session[:user_role], "delete_instructor")
+      redirect_to root_path
+    end
     @instructor.destroy
-    # TODO check permissions
+
     respond_to do |format|
       format.html { redirect_to instructors_url, notice: "Instructor was successfully destroyed." }
       format.json { head :no_content }
@@ -72,14 +88,13 @@ class InstructorsController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_instructor
+      @instructor = Instructor.find(params[:id])
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_instructor
-    @instructor = Instructor.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def instructor_params
-    params.require(:instructor).permit(:instructor_id, :name, :email, :password, :department)
-  end
+    # Only allow a list of trusted parameters through.
+    def instructor_params
+      params.require(:instructor).permit(:instructor_id, :name, :email, :password, :department)
+    end
 end
