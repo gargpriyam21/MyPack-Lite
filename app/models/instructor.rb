c753d1a2c2846305312e75f084366c667558f235
Dev Mehta <devmehta5799@gmail.com>
2022-02-13 16:18:04 -0500

    Implemented Course and Instructor Validations

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,6 +2,13 @@ class Instructor < ApplicationRecord
   has_secure_password
   has_many :students
   has_many :courses, dependent: :destroy
-  validates :email, presence: true, uniqueness: true
+  validates :email,:instructor_id, presence: true, uniqueness: true
   belongs_to :user, dependent: :destroy
+  validates :name, :password_digest, :department, presence: true
+
+  validates :email, format: {
+    with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i,
+    message: "Invalid Email",
+    multiline: true
+  }
 end
