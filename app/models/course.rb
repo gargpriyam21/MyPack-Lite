f1502d80f0850fc62a0c4372d1863b4670ae9ddb
Priyam Garg <gargpriyam21@gmail.com>
2022-02-13 00:45:41 -0500

    Enrollments Foreign Key Update

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,7 @@
 class Course < ApplicationRecord
+  has_one :instructor
+  has_many :enrollments, dependent: :destroy
+  has_many :students
+  belongs_to :student, optional: true
+  belongs_to :instructor
 end
