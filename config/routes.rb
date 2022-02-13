f7dc375493b9d5f0a57296ab6dfaf169701f5dc3
Dev Mehta <devmehta5799@gmail.com>
2022-02-13 15:50:19 -0500

    Added My Students Functionality for Instructor

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,18 @@ Rails.application.routes.draw do
   get 'instructor_signup', to: "instructors#new", as: 'instructor_signup'
   get 'login', to: "sessions#new", as: 'login'
   get 'logout', to: "sessions#destroy", as: 'logout'
-  get 'mycourses', to: "courses#show_instructor_courses", as: "mycourses"
-  get 'mycourses', to: "courses#showinstructorcourses", as: "mycourses"
+  get 'instructor_courses', to: "courses#show_instructor_courses", as: "instructor_courses"
   get 'student_enrollments', to: "courses#show_student_enrolled_courses", as: "student_enrollments"
+  get 'show_instructor_students_enrolled',  to: "enrollments#show_instructor_students_enrolled", as: "show_instructor_students_enrolled"
+
   resources :courses do
     member do
       get 'enroll'
+    end
+  end
+  resources :enrollments do
+    member do
+      get 'unenroll'
     end
   end
   resources :courses do
