0eb2b6cf15b268596593bb75c9f33b4dbfa2cf3b
mrjain <mrjain@ncsu.edu>
2022-02-13 20:16:09 -0500

    updated views

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -65,6 +65,7 @@ class AdminsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def admin_params
-      params.require(:admin).permit(:admin_id, :password, :name, :email, :phone_number)
+      # params.require(:admin).permit(:admin_id, :password, :name, :email, :phone_number)
+      params.require(:admin).permit(:id, :name, :phone_number)
     end
 end
