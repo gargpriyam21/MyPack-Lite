da394b25f389d099de236ed754a94372a6440b4b
Dev Gaurangbhai Mehta <dmehta3@ncsu.edu>
2022-02-21 21:19:47 -0500

    Bug Fixes

--- test/models/enrollment_test.rb
+++ test/models/enrollment_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class EnrollmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not create application with null values" do
+    @enrollment = Enrollment.new
+    assert_not @enrollment.save
+  end
 end
+
+
