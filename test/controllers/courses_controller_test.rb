04ecd5c7b9c52b54015466560fa737e4f124ec3c
Dev Mehta <devmehta5799@gmail.com>
2022-02-21 21:13:55 -0500

    Bug Fixes

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -51,47 +51,47 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
     assert_response :error
   end
 
-  test "should not create curse with negative capacity" do
+  test "should not create course with negative capacity" do
     post courses_url, params: { course: { capacity: -10, course_code: @course.course_code, description: @course.description, end_time: @course.end_time, instructor_name: @course.instructor_name, name: @course.name, room: @course.room, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
 
-  test "should not create curse with null name only" do
+  test "should not create course with null name only" do
     post courses_url, params: { course: { capacity: @course.capacity, course_code: @course.course_code, description: @course.description, end_time: @course.end_time, instructor_name: @course.instructor_name, room: @course.room, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
 
-  test "should not create curse with null description only" do
+  test "should not create course with null description only" do
     post courses_url, params: { course: { capacity: @course.capacity, course_code: @course.course_code, end_time: @course.end_time, instructor_name: @course.instructor_name, name: @course.name, room: @course.room, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
 
-  test "should not create curse with null course code only" do
+  test "should not create course with null course code only" do
     post courses_url, params: { course: { capacity: @course.capacity, description: @course.description, end_time: @course.end_time, instructor_name: @course.instructor_name, name: @course.name, room: @course.room, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
 
-  test "should not create curse with null room only" do
+  test "should not create course with null room only" do
     post courses_url, params: { course: { capacity: @course.capacity, course_code: @course.course_code, description: @course.description, end_time: @course.end_time, instructor_name: @course.instructor_name, name: @course.name, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
 
-  test "should not create curse with invalid course_code" do
+  test "should not create course with invalid course_code" do
     post courses_url, params: { course: { capacity: @course.capacity, course_code: "CSDSC63682", description: @course.description, end_time: @course.end_time, instructor_name: @course.instructor_name, name: @course.name, room: @course.room, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
 
-  test "should not create curse with weekday1 equal to weekday2" do
+  test "should not create course with weekday1 equal to weekday2" do
     post courses_url, params: { course: { capacity: @course.capacity, course_code: @course.course_code, description: @course.description, end_time: @course.end_time, instructor_name: @course.instructor_name, name: @course.name, room: @course.room, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: "MON", weekday2: "MON" } }
     assert_response :error
   end
 
-  test "should not create curse with end_time less that end_time only" do
+  test "should not create course with end_time less than start_time only" do
     post courses_url, params: { course: { capacity: @course.capacity, course_code: @course.course_code, description: @course.description, end_time: "12:54", instructor_name: @course.instructor_name, name: @course.name, room: @course.room, start_time: "15:43", status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: @course.waitlist_capacity, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
 
-  test "should not create curse with negative waitlist_capacity only" do
+  test "should not create course with negative waitlist_capacity only" do
     post courses_url, params: { course: { capacity: @course.capacity, course_code: @course.course_code, description: @course.description, end_time: @course.end_time, instructor_name: @course.instructor_name, name: @course.name, room: @course.room, start_time: @course.start_time, status: @course.status, students_enrolled: @course.students_enrolled, students_waitlisted: @course.students_waitlisted, waitlist_capacity: -120, weekday1: @course.weekday1, weekday2: @course.weekday2 } }
     assert_response :error
   end
